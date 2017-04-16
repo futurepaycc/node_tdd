0081d5f0fce51a3ad44116798d68e929a403feea
story哥 <liuyifan.com@163.com>
2017-04-16 14:22:56 +0800

    增加readme

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+function fib(n){
+    if(n<4) return n;
+    return fib(n-1) + fib(n-2)
+}
+
 function load(){
     console.log("hello world");
+    console.log("这个代码编辑器还不错");
 }
 
 //这里的程序如何debug?
+function runFib(){
+    for(i=0;i<100;i++){
+        console.log(i+"---"+fib(i))
+    }
+}
