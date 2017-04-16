66844384c07bd7d5198ba1f0592f16021bb1079d
147403 <liuhui@syswin.com>
2017-04-17 00:21:49 +0800

    post not good

--- test/user.spec.js
+++ test/user.spec.js
@@ -27,9 +27,25 @@ describe('user tests', function () {
     })
 
     describe('GET /hello', function () {
-        it('should return all shows', async function () {
+        it('result show be true', async function () {
             let res = await chai.request(server).get('/hello')
             res.body.result.should.equal(true)
+        })
+    })
+
+    describe('POST /login', function () {
+        it('login should sucess', async function () {
+            try {
+                let res = await chai.request(server).post('/login').send({ 'name': 'liunix', 'password': '123456' })
+                // console.log('res=',res)
+                // console.log('res.body=',res.body)
+                // res.body.result.should.equal(true)
+                // let res = await chai.request(server).post('/login').field('_method', 'put').field('name','liunix').field('password','123456')
+                // res.body.result.should.equal(true)
+                console.log(res)
+            } catch (err) {
+                console.log(err)
+            }
         })
     })
 
